f9df3b377efd36846cbf53d5e0bf10a6b721b56c
Filipe Fernandes <ocefpaf@gmail.com>
2018-07-27 09:55:37 -0300

    update build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-sed -i.orig s:'@PREFIX@':"${PREFIX}":g src/fccfg.c
-
 # Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
 # process of excising Libtool files from our packages. Existing ones can break
 # the build while this happens.
 find $PREFIX -name '*.la' -delete
 
-autoreconf -f -i
+sed -i.orig s:'@PREFIX@':"${PREFIX}":g src/fccfg.c
 
-./configure --prefix "${PREFIX}" \
-	    --enable-libxml2 \
-	    --enable-static \
-	    --disable-docs \
-      --with-add-fonts=${PREFIX}/fonts
+# So that -Wl,--as-needed works (sorted to appear before before libs)
+autoreconf -vfi
 
-make -j${CPU_COUNT}
-make check
+# See:
+# https://github.com/Homebrew/homebrew-core/blob/master/Formula/fontconfig.rb
+# https://bugs.freedesktop.org/show_bug.cgi?id=105366
+if [[ ${target_platform} == osx-64 ]]; then
+  export UUID_CFLAGS=" "
+  export UUID_LIBS=" "
+  sed -i -e 's|PKGCONFIG_REQUIRES_PRIVATELY=\"\$PKGCONFIG_REQUIRES_PRIVATELY uuid\"||g' configure
+  FONT_DIRS=--with-add-fonts="${PREFIX}"/fonts,/System/Library/Fonts,/Library/Fonts,~/Library/Fonts,/System/Library/Assets/com_apple_MobileAsset_Font3,/System/Library/Assets/com_apple_MobileAsset_Font4
+else
+  FONT_DIRS=--with-add-fonts="${PREFIX}"/fonts
+fi
+
+./configure --prefix="${PREFIX}"                \
+            --enable-libxml2                    \
+            --enable-static                     \
+            --disable-docs                      \
+            "${FONT_DIRS}"
+
+
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make check ${VERBOSE_AT}
 make install
 
-# Remove any new Libtool files we may have installed. It is intended that
-# conda-build will eventually do this automatically.
-find $PREFIX -name '*.la' -delete
-
 # Remove computed cache with local fonts
-rm -Rf "${PREFIX}/var/cache/fontconfig"
+rm -Rf "${PREFIX}"/var/cache/fontconfig
 
 # Leave cache directory, in case it's needed
-mkdir -p "${PREFIX}/var/cache/fontconfig"
-touch "${PREFIX}/var/cache/fontconfig/.leave"
+mkdir -p "${PREFIX}"/var/cache/fontconfig
+touch "${PREFIX}"/var/cache/fontconfig/.leave
