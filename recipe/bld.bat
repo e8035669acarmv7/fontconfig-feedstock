3f27719a3eec46095ad3076d7632cb718fe79371
Peter Williams <peter@newton.cx>
2022-03-03 22:54:44 +0000

    Use Meson to build on windows
    This simplifies the build greatly, gives us a shared library, and eliminates the
    need for many patches.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,27 @@
-:: Trailing semicolon in this variable as set by current (2017/01)
-:: conda-build breaks us. Manual fix:
-set "MSYS2_ARG_CONV_EXCL=/AI;/AL;/OUT;/out"
-:: Delegate to the Unixy script. We need to translate the key path variables
-:: to be Unix-y rather than Windows-y, though.
-set "saved_recipe_dir=%RECIPE_DIR%"
-FOR /F "delims=" %%i IN ('cygpath.exe -u -p "%PATH%"') DO set "PATH_OVERRIDE=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -m "%BUILD_PREFIX%"') DO set "BUILD_PREFIX_M=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -m "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_M=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%LIBRARY_PREFIX%"') DO set "LIBRARY_PREFIX_U=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PREFIX%"') DO set "PREFIX=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%PYTHON%"') DO set "PYTHON=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%RECIPE_DIR%"') DO set "RECIPE_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SP_DIR%"') DO set "SP_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
-FOR /F "delims=" %%i IN ('cygpath.exe -u "%STDLIB_DIR%"') DO set "STDLIB_DIR=%%i"
-bash -x %saved_recipe_dir%\build.sh
+setlocal EnableDelayedExpansion
+@echo on
+
+set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
+
+set ^"MESON_OPTIONS=^
+  --prefix="%LIBRARY_PREFIX%" ^
+  --default-library=shared ^
+  --wrap-mode=nofallback ^
+  --buildtype=release ^
+  --backend=ninja ^
+ ^"
+
+meson setup builddir !MESON_OPTIONS!
+if errorlevel 1 exit 1
+
+meson configure builddir
+if errorlevel 1 exit 1
+
+ninja -v -C builddir -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+ninja -v -C builddir test -j %CPU_COUNT%
+if errorlevel 1 exit 1
+
+ninja -C builddir install -j %CPU_COUNT%
 if errorlevel 1 exit 1
