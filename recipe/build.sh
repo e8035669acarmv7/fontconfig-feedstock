615def7280b1f0ac13c8b0b82602183258d2c468
Peter Williams <peter@newton.cx>
2022-03-04 11:03:39 -0500

    Set up for osx-arm64 build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,27 @@ set -xeo pipefail
 # For the Linux builds, get our prefix into the shared library
 sed -i.orig s:'@PREFIX@':"$PREFIX":g src/fccfg.c
 
-meson setup builddir \
-    --default-library=both \
-    --buildtype=release \
-    --prefix=$PREFIX \
-    -Dlibdir=lib \
+# osx-arm64 cross build sets up $MESON_ARGS with these settings plus the cross file:
+if [[ $MESON_ARGS == "" ]] ; then
+    MESON_ARGS="--buildtype=release --prefix=$PREFIX -Dlibdir=lib"
+fi
+
+if [[ "$target_platform" == "osx-arm64" && "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+    export PKG_CONFIG=$BUILD_PREFIX/bin/pkg-config
+
+    # When cross-compiling the environment is set up as if Python will be used
+    # as a host/run tool, but for us it is genuinely just a build tool. This
+    # override fixes Meson's detection of Python given that:
+    export _CONDA_PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata_x86_64_apple_darwin13_4_0
+fi
+
+meson_setup_args=(
+    $MESON_ARGS
+    --default-library=both
     --wrap-mode=nofallback
+)
+
+meson setup builddir "${meson_setup_args[@]}" || { cat builddir/meson-logs/meson-log.txt ; exit 1 ; }
 
 ninja -v -C builddir -j ${CPU_COUNT}
 
